f5ffc580bd159d21e3a9fe51a05293ee1d2773fa
Aidan Wolter <ajohnwoo@aol.com>
2013-04-15 13:38:36 -0500

    untracked files

---
+++ iLETU/src/com/example/iletu/AnnouncementsFragment.java
@@ -0,0 +1,8 @@
+package com.example.iletu;
+
+public class AnnouncementsFragment extends NewsFragment {
+	public AnnouncementsFragment() {
+		super();
+		setUrl("http://letustartpage.blogspot.com/feeds/posts/default?alt=rss");
+	}
+}
