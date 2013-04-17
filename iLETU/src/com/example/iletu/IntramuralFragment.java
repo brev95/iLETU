81fd47b72e20df7d75e2678aece1f4af9c941dc1
Aidan Wolter <ajohnwoo@aol.com>
2013-04-16 23:41:19 -0500

    fixed transitions

--- iLETU/src/com/example/iletu/IntramuralFragment.java
+++ iLETU/src/com/example/iletu/IntramuralFragment.java
@@ -16,6 +16,7 @@ import android.webkit.WebViewClient;
 public class IntramuralFragment extends Fragment{
 	private String url = "http://www.imleagues.com/School/Portal.aspx?SchID=39d7b8cb08db465ab4f27a061607a7c4&Portal=SchOfDay";
 	private WebView wv;
+	String html;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,17 +32,23 @@ public class IntramuralFragment extends Fragment{
 		wv.getSettings().setBuiltInZoomControls(true);
         wv.getSettings().setSupportZoom(true);
 		
-        org.jsoup.nodes.Document doc;
-		try {
-			doc = Jsoup.connect(url).get();
-			Elements ele = doc.select("div#divGameSearchResult");
-			String html = ele.toString();
-			String mime = "text/html";
+        if (html == null) {
+	        org.jsoup.nodes.Document doc;
+			try {
+				doc = Jsoup.connect(url).get();
+				Elements ele = doc.select("div#divGameSearchResult");
+				String html = ele.toString();
+				String mime = "text/html";
+				String encoding = "utf-8";
+				wv.loadData(html, mime, encoding);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        } else {
+        	String mime = "text/html";
 			String encoding = "utf-8";
 			wv.loadData(html, mime, encoding);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        }
         
 		return view;
 	}
