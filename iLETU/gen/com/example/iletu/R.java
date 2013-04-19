f9ecd99aba831861a41d30a5fade7785ba82cf70
awp07 <andrew.w.pollard@gmail.com>
2013-04-18 19:41:48 -0500

    added imageview in campusbullet fragment

--- iLETU/gen/com/example/iletu/R.java
+++ iLETU/gen/com/example/iletu/R.java
@@ -24,15 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
         public static final int details=0x7f080003;
-        public static final int listMainView=0x7f080004;
-        public static final int mLine1=0x7f080005;
-        public static final int mLine2=0x7f080006;
+        public static final int image_view=0x7f080004;
+        public static final int listMainView=0x7f080005;
+        public static final int mLine1=0x7f080006;
+        public static final int mLine2=0x7f080007;
         public static final int pager=0x7f080000;
         public static final int pager_header=0x7f080001;
         public static final int title=0x7f080002;
-        public static final int webview=0x7f080007;
+        public static final int webview=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- iLETU/src/com/example/iletu/DetailActivity.java
+++ iLETU/src/com/example/iletu/DetailActivity.java
@@ -1,13 +1,25 @@
 package com.example.iletu;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.webkit.URLUtil;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends Activity{
 	Intent intent;
 	TextView tView, dView, lView;
+	ImageView imgView;
+	String link, postNum, baseUrl, finalUrl;
+	Drawable drawable;
+	URL url;
+    InputStream in = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +33,38 @@ public class DetailActivity extends Activity{
 		
 		tView.setText(intent.getExtras().getString("title"));
 		dView.setText(intent.getExtras().getString("description"));
-		//lView.setText(intent.getExtras().getString("link"));
+		link = intent.getExtras().getString("link");
+		
+		imgView =(ImageView)findViewById(R.id.image_view);
+		baseUrl= "http://www.campusbullet.net/images/posts/?q=";
+		postNum = link.substring(38);
+		//lView.setText(link);
+		
+		try {
+			finalUrl = baseUrl+postNum+"-1";
+			url = new URL(finalUrl);
+			if(URLUtil.isValidUrl(finalUrl)); {
+				drawable =LoadImageFromWeb(url);
+				imgView.setImageDrawable(drawable);	
+			}
+		} catch(MalformedURLException e) {
+			System.out.println("Exc= "+e);
+		}
 	}
 	
 	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
 		super.onDestroy();
+	}
+	
+	private Drawable LoadImageFromWeb(URL url) {
+		try {
+			in = (InputStream)url.getContent();
+			Drawable d = Drawable.createFromStream(in, "src name");
+			return d;
+		} catch(Exception e) {
+			return null;
+		}
 	}
 }
