30a3fcc50eb8a2ae30f4625ca77a06d0459a767f
awp07 <andrew.w.pollard@gmail.com>
2013-04-22 09:46:41 -0500

    commit to try to merge with fixed parser

--- iLETU/gen/com/example/iletu/R.java
+++ iLETU/gen/com/example/iletu/R.java
@@ -24,7 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int about_app=0x7f08000b;
         public static final int details=0x7f080003;
         public static final int image_view=0x7f080004;
         public static final int linksMainView=0x7f080005;
--- iLETU/src/com/example/iletu/MainActivity.java
+++ iLETU/src/com/example/iletu/MainActivity.java
@@ -123,11 +123,11 @@ public class MainActivity extends FragmentActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
     
 }
